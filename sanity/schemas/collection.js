40c9c35b2b4a79f868dd8732389366cd7ea87ba0
RiP3rQ <rafalpompa2000@gmail.com>
2023-02-25 16:24:33 +0100

    sanity setup

---
+++ sanity/schemas/collection.js
@@ -0,0 +1,61 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'collection',
+  title: 'Collection',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      description: 'Enter the Title of the NFT Drop',
+      type: 'string',
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+    }),
+    defineField({
+      name: 'nftCollectionName',
+      title: 'Name of NFT Collection',
+      type: 'string',
+    }),
+    defineField({
+      name: 'address',
+      title: 'Address',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'creator',
+      title: 'Creator',
+      type: 'reference',
+      to: {type: 'creator'},
+    }),
+    defineField({
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'previewImage',
+      title: 'Preview image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+  ],
+})
